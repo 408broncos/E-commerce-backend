9c618621c7903b031550e894e3955401acb7bc65
Jordan Cardenas <jordancardenas@Jordans-MBP.attlocal.net>
2023-05-03 18:11:05 -0700

    add all columns for js fi les in models folder, add .env

--- models/Category.js
+++ models/Category.js
@@ -1,5 +1,4 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
@@ -7,10 +6,20 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
-    timestamps: false,
+    timestamps: true,
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
